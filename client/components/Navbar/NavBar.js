788dd188e991de389ece1e9224d1ea719ab7041a
Fatema04 <shadrinmoni15@gmail.com>
2023-01-01 23:40:30 +0600

    navbar completed

--- client/components/Navbar/NavBar.js
+++ client/components/Navbar/NavBar.js
@@ -7,20 +7,20 @@ const NavBar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     return (
-        <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-            <div class="relative flex items-center justify-between">
+        <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+            <div className="relative flex items-center justify-between">
                 <a
                     href="/"
                     aria-label="Company"
                     title="Company"
-                    class="inline-flex items-center"
+                    className="inline-flex items-center"
                 >
-                    <Image src='/assets/images/logo3.png' height={60} width={60}></Image>
-                    <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
+                    <Image src='/assets/images/logo3.png' height={60} width={60} alt='logo'></Image>
+                    <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
                         Skyfly Tickets
                     </span>
                 </a>
-                <ul class="flex items-center hidden space-x-8 lg:flex text-xl text-gray-900 text-semibold">
+                <ul className="flex items-center hidden space-x-8 lg:flex text-xl text-gray-900 text-semibold">
                     <li>
                         <Link href=''>Home</Link>
                     </li>
@@ -34,28 +34,28 @@ const NavBar = () => {
                         <Link href=''>Contact</Link>
                     </li>
                 </ul>
-                <ul class="flex items-center hidden space-x-8 lg:flex">
+                <ul className="flex items-center hidden space-x-8 lg:flex">
                     <li className="text-xl text-gray-900">
                         <Link href=''>Sign in</Link>
                     </li>
                     <li>
-                        <a href="#_" class="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-pink-700 rounded-xl group">
-                            <span class="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-pink-900 rounded group-hover:-mr-4 group-hover:-mt-4">
-                                <span class="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
+                        <a href="#_" className="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-pink-700 rounded-xl group">
+                            <span className="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-pink-900 rounded group-hover:-mr-4 group-hover:-mt-4">
+                                <span className="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
                             </span>
-                            <span class="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full translate-y-full bg-pink-600 rounded-2xl group-hover:mb-12 group-hover:translate-x-0"></span>
-                            <span class="relative w-full text-left text-white text-xl transition-colors duration-200 ease-in-out group-hover:text-white">Sign up</span>
+                            <span className="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full translate-y-full bg-pink-600 rounded-2xl group-hover:mb-12 group-hover:translate-x-0"></span>
+                            <span className="relative w-full text-left text-white text-xl transition-colors duration-200 ease-in-out group-hover:text-white">Sign up</span>
                         </a>
                     </li>
                 </ul>
-                <div class="lg:hidden">
+                <div className="lg:hidden">
                     <button
                         aria-label="Open Menu"
                         title="Open Menu"
-                        class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
+                        className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
                         onClick={() => setIsMenuOpen(true)}
                     >
-                        <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+                        <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
                             <path
                                 fill="currentColor"
                                 d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
@@ -71,19 +71,19 @@ const NavBar = () => {
                         </svg>
                     </button>
                     {isMenuOpen && (
-                        <div class="absolute top-0 left-0 w-full">
-                            <div class="p-5 bg-white border rounded shadow-sm">
-                                <div class="flex items-center justify-between mb-4">
+                        <div className="absolute top-0 left-0 w-full">
+                            <div className="p-5 bg-white border rounded shadow-sm">
+                                <div className="flex items-center justify-between mb-4">
                                     <div>
                                         <a
                                             href="/"
                                             aria-label="Company"
                                             title="Company"
-                                            class="inline-flex items-center"
+                                            className="inline-flex items-center"
                                         >
-                                            <Image src='/assets/images/logo3.png' height={50} width={50}></Image>
+                                            <Image src='/assets/images/logo3.png' height={50} width={50} alt='logo'></Image>
 
-                                            <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
+                                            <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
                                                 Skyfly Tickets
                                             </span>
                                         </a>
@@ -92,10 +92,10 @@ const NavBar = () => {
                                         <button
                                             aria-label="Close Menu"
                                             title="Close Menu"
-                                            class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                                            className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
                                             onClick={() => setIsMenuOpen(false)}
                                         >
-                                            <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+                                            <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
                                                 <path
                                                     fill="currentColor"
                                                     d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
@@ -105,7 +105,7 @@ const NavBar = () => {
                                     </div>
                                 </div>
                                 <nav>
-                                    <ul class="space-y-4 text-xl text-gray-900 text-semibold">
+                                    <ul className="space-y-4 text-xl text-gray-900 text-semibold">
                                         <li>
                                             <Link href=''>Home</Link>
                                         </li>
@@ -123,12 +123,12 @@ const NavBar = () => {
                                             <Link href=''>Sign in</Link>
                                         </li>
                                         <li>
-                                            <a href="#_" class="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-pink-700 rounded-xl group">
-                                                <span class="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-pink-900 rounded group-hover:-mr-4 group-hover:-mt-4">
-                                                    <span class="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
+                                            <a href="#_" className="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-pink-700 rounded-xl group">
+                                                <span className="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-pink-900 rounded group-hover:-mr-4 group-hover:-mt-4">
+                                                    <span className="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
                                                 </span>
-                                                <span class="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full translate-y-full bg-pink-600 rounded-2xl group-hover:mb-12 group-hover:translate-x-0"></span>
-                                                <span class="relative w-full text-left text-white text-xl transition-colors duration-200 ease-in-out group-hover:text-white">Sign up</span>
+                                                <span className="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full translate-y-full bg-pink-600 rounded-2xl group-hover:mb-12 group-hover:translate-x-0"></span>
+                                                <span className="relative w-full text-left text-white text-xl transition-colors duration-200 ease-in-out group-hover:text-white">Sign up</span>
                                             </a>
                                         </li>
                                     </ul>
