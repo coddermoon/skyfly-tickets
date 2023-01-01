6bc2fa13a76753384049a6bb524fe0a8c3cae3f0
Fatema04 <shadrinmoni15@gmail.com>
2023-01-02 00:20:13 +0600

    navbar complete

--- client/components/Navbar/NavBar.js
+++ client/components/Navbar/NavBar.js
@@ -39,13 +39,13 @@ const NavBar = () => {
                         <Link href=''>Sign in</Link>
                     </li>
                     <li>
-                        <a href="#_" className="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-pink-700 rounded-xl group">
+                        <Link href="/signup" className="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-pink-700 rounded-xl group">
                             <span className="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-pink-900 rounded group-hover:-mr-4 group-hover:-mt-4">
                                 <span className="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
                             </span>
                             <span className="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full translate-y-full bg-pink-600 rounded-2xl group-hover:mb-12 group-hover:translate-x-0"></span>
                             <span className="relative w-full text-left text-white text-xl transition-colors duration-200 ease-in-out group-hover:text-white">Sign up</span>
-                        </a>
+                        </Link>
                     </li>
                 </ul>
                 <div className="lg:hidden">
