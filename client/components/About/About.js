172e6f3638c4ac5ff72bbab450697018fa485e89
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-01-02 22:44:16 -0800

    about button edited

--- client/components/About/About.js
+++ client/components/About/About.js
@@ -32,16 +32,17 @@ const About = () => {
             soluta exercitationem. Cupiditate, maiores.
           </p>
 
-          <a href="#_" class="relative inline-block text-lg group">
-            <span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
-              <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
-              <span class="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
-              <span class="relative">Button Text</span>
+          <a
+            href="#_"
+            className="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-pink-700 rounded-xl group"
+          >
+            <span className="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-pink-900 rounded group-hover:-mr-4 group-hover:-mt-4">
+              <span className="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
             </span>
-            <span
-              class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
-              data-rounded="rounded-lg"
-            ></span>
+            <span className="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full translate-y-full bg-pink-600 rounded-2xl group-hover:mb-12 group-hover:translate-x-0"></span>
+            <span className="relative w-full text-left text-white text-xl transition-colors duration-200 ease-in-out group-hover:text-white">
+              More About us
+            </span>
           </a>
         </div>
       </div>
